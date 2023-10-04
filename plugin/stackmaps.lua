e08080b1b7ed15aaf779fed08ad29a8ae2e42c28
Isaac Turner <spacey_sooty@outlook.com>
2023-10-04 13:19:24 +0800

    add find_map

--- plugin/stackmaps.lua
+++ plugin/stackmaps.lua
@@ -6,14 +6,32 @@ local keymap = vim.keymap
 -- vim.keymap.set()
 -- vim.api.nvim_get_keymap()
 
-M.push = function(name, mappings) 
+local find_mapping = function(maps, lhs)
+	-- pairs iterates over all keys in table order not guaranteed
+	-- ipairs iterates over all numeric values in table order guaranteed
+	for _, value in ipairs(maps) do
+		if value.lhs == lhs
+			return value
+		end
+	end
+
+M.push = function(name, mode, mappings) 
+	local maps = api.nvim_get_keymap(mode)
+
+	find_mapping(maps, " pf")
 end
 
--- temporarily create debug mode keybinding in normal mode
+-- temporarily create keybindings in normal mode with name debug_mode
 -- M.push('debug_mode', 'n', {
 -- 	["<leader>st"] = "echo 'Hello'",
 -- 	["<leader>sz"] = "echo 'Lol'",
 -- })
+
+-- test
+M.push('debug_mode', 'n', {
+	[" pf"] = "echo 'Hello'",
+ 	[" sz"] = "echo 'Lol'",
+})
 
 M.pop = function(name)
 end
