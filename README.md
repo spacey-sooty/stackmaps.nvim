b4f7fd65c530249730942087d38557df5d454cc5
Isaac Turner <spacey_sooty@outlook.com>
2023-10-04 12:59:20 +0800

    add useful comments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # stackmaps.nvim
 A follow along to this video https://www.youtube.com/watch?v=n4Lp4cV8YR0&amp;ab_channel=TJDeVries
+
--- plugin/stackmaps.lua
+++ plugin/stackmaps.lua
@@ -1,10 +1,22 @@
 local M = {}
+local api = vim.api
+local keymap = vim.keymap
+
+-- functions we need:
+-- vim.keymap.set()
+-- vim.api.nvim_get_keymap()
 
 M.push = function(name, mappings) 
 end
 
+-- temporarily create debug mode keybinding to dm
+-- M.push('debug_mode', dm)
+
 M.pop = function(name)
 end
+
+-- reset debug mode keybinding
+-- M.pop('debug_mode')
 
 return M
 
